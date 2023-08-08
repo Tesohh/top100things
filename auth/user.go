2a50f7329ac94a7046deaa8b5379082eb5580a78
Tesini <Simone.Tesini.ext@wuerth-phoenix.com>
2023-08-08 12:29:49 +0200

    fix(User): fix an issue where user was being always refreshed

--- auth/user.go
+++ auth/user.go
@@ -2,7 +2,6 @@ package auth
 
 import (
 	"context"
-	"log"
 	"net/http"
 
 	"github.com/Tesohh/top100things/sb"
@@ -12,15 +11,19 @@ import (
 func User(r *http.Request) *supabase.User {
 	ac, aerr := r.Cookie("AccessToken")
 	rc, rerr := r.Cookie("RefreshToken")
-	if aerr == nil && rerr == nil { // if the cookie exists
-		// user, err := sb.SB.Auth.User(context.Background(), ac.Value)
-		ad, err := sb.SB.Auth.RefreshUser(context.Background(), ac.Value, rc.Value)
-		if err == nil {
-			return &ad.User
-		} else {
-			log.Println("user: " + err.Error())
-		}
+	_ = rc
+	if aerr != nil || rerr != nil { // cookies don't exist == timed out or logged out
+		return nil
 	}
-	return nil
 
+	user, err := sb.SB.Auth.User(context.Background(), ac.Value)
+	if user == nil || err != nil { // the user cannot be retrieved, probably due to the token being timed out
+		ad, err := sb.SB.Auth.RefreshUser(context.Background(), ac.Value, rc.Value)
+		if err != nil {
+			return nil
+		}
+		user = &ad.User
+	}
+
+	return user
 }
